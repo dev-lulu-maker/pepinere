b6a50f0ea31898f7da611ffdef08b60413d7ddd1
dev-lulu-maker <zhonghaiying251327@gmail.com>
2025-07-09 07:15:53 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,36 @@
+// const { createElement } = require("react");
+
+const images = [
+  'medias/images/hero1.jpg',
+  'medias/images/hero2.jpg',
+  'medias/images/hero3.jpg'
+];
+
+let currentIndex = 0;
+const hero = document.querySelector('.hero-carousel');
+
+function changeBackground() {
+  hero.style.backgroundImage = `url('${images[currentIndex]}')`;
+  currentIndex = (currentIndex + 1) % images.length;
+}
+
+// Lancer au chargement
+changeBackground();
+
+// Changer toutes les 5 secondes
+setInterval(changeBackground, 5000);
+
+
+const btn = document.getElementById("menuToggle")
+const navbar = document.querySelector('.navbar')
+const btnClose= document.querySelector('.btnClose')
+
+btn.addEventListener("click", () => {
+  navbar.classList.add('active');
+  btnClose.addEventListener('click', () => {
+    navbar.classList.remove('active')
+  })
+})
+
+
+
